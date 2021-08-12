02d8a3094793063b36d22920e828aede9ee0a167
juanseferrari <juansegundoferrari@gmail.com>
2021-08-11 21:25:48 -0300

    agregado forms

--- mercado-liebre/app.js
+++ mercado-liebre/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express()
 
 
-app.listen(3000, ()=>{
+app.listen(8000, ()=>{
     console.log("Servidor prendido")
 })
 
@@ -12,5 +12,11 @@ app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/", (req,res)=> {
     res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+app.get("/register", (req,res)=> {
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+})
+app.get("/login", (req,res)=> {
+    res.sendFile(path.join(__dirname,'/views/login.html'))
 })
 
