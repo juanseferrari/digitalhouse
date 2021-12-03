26d14fe8cb98bea2d90bd46ad44acbda45393a03
juanseferrari <juansegundoferrari@gmail.com>
2021-12-03 09:17:46 -0300

    agregado react

--- mod8-js-frontend-ajax/src/database/config/config.js
+++ mod8-js-frontend-ajax/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'MySQL3127',
+    "password": null,
     "database": "movies_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
